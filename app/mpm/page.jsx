4872e7e678e76463a8e88bc68de71d2ce11f1f9c
Jona Metcalf <jonametcalf@outlook.com>
2023-11-26 19:25:58 +0000

    Added point cloud

--- app/mpm/page.jsx
+++ app/mpm/page.jsx
@@ -1,18 +1,29 @@
 'use client'
-import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react'
 import { Canvas } from '@react-three/fiber'
+import {useRef} from 'react'
+import { OrbitControls } from '@react-three/drei';
+
 
 export default function App() {
+  const points = useRef();
+  const particlesPosition = new Float32Array([0,0,0,3,1,1]);
   return (
     <div id="canvas-container">
-      <Canvas>
-    <pointLight position={[10, 10, 10]} />
-    <mesh>
-      <sphereGeometry />
-      <meshStandardMaterial color="hotpink" />
-    </mesh>
-  </Canvas>
+      <Canvas camera={{ fov: 75, position: [3, 3, 3]}}>
+        <OrbitControls autoRotate enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} enablePan={true} />
+        <pointLight position={[1, 5, 1]} />
+        <points ref={points}>
+          <bufferGeometry>
+            <bufferAttribute
+              attach="attributes-position"
+              count={particlesPosition.length / 3}
+              array={particlesPosition}
+              itemSize={3}
+            />
+            </bufferGeometry>
+          <pointsMaterial color="#5786F5" size={2} sizeAttenuation />
+        </points>
+      </Canvas>
     </div>
   )
 }
