da60804288adb38c51ba9159191d68c82fb4efe0
Jona Metcalf <jonametcalf@outlook.com>
2023-11-28 17:12:26 +0000

    Added slider

--- app/mpm/page.jsx
+++ app/mpm/page.jsx
@@ -2,18 +2,22 @@
 import { Canvas, useFrame} from '@react-three/fiber'
 import {useRef, useState, useEffect} from 'react'
 import { OrbitControls } from '@react-three/drei';
+import { Slider } from "@material-tailwind/react";
+
 
 
 export default function App() {
   const [data, setData] = useState();
-  const [counter, setCounter] = useState(1);
+  const [framechoice, setFrameChoice] = useState(0);
+  const [frames, setFrames] = useState();
 
   const MAX_POINTS = 43000
   const [coords, setCoords] = useState(new Float32Array(MAX_POINTS * 3))
 
   const points = useRef();
   const box = useRef();
-  const leftwall = useRef();
+  const floor = useRef();
+  const backwall = useRef();
   
 
   useEffect(() => {
@@ -21,32 +25,37 @@ export default function App() {
       const data = await (await fetch('/3d.json')).json();
       setData(data);
       console.log("data fetched")
-      points.current.geometry.attributes.position.array = new Float32Array(data[counter]["data"])
+      setFrames(Array.from(Object.keys(data)))
+      points.current.geometry.attributes.position.array = new Float32Array(data[Object.keys(data)[0]]["data"])
       points.current.geometry.attributes.position.needsUpdate = true
       
     };
     dataFetch();
   }, []);
 
-  function loadFrame () {
-    points.current.geometry.attributes.position.array = new Float32Array(data[counter]["data"])
-    points.current.geometry.computeBoundingBox();
+  function loadFrame (frame) {
+    points.current.geometry.attributes.position.array = new Float32Array(data[frame.toString()]["data"])
+    //points.current.geometry.computeBoundingBox(); fix Nan values in array 
     points.current.geometry.attributes.position.needsUpdate = true
-
   }
 
   return (
-    <div>
-      <button onClick={() => {setCounter(counter+1); loadFrame()} }>
-        increase
-      </button>
-      <button onClick={() => {setCounter(counter-1); loadFrame()} }>
-        decrease
-      </button>
-      <p>counter at {counter}</p>
-      <div id="canvas-container" className='w-300 h-screen'>
+    <div className='w-full h-full py-20 px-5 justify-center'>
+      <h1 className='text-4xl mx-auto'>MPM modelling of subaerial and underwater landslides</h1>
+      <div className='w-4/6 mx-auto outline outline-1 p-3'>
+      <input
+            type="range"
+            className="transparent h-1.5 w-60 cursor-pointer appearance-none rounded-lg border-transparent bg-neutral-200"
+            min="0"
+            max="120"
+            step="4"
+            defaultValue={0}
+            onChange={({ target: { value: frame } }) => {loadFrame(frame)}}
+          />
+      <div id="canvas-container" className='w-full h-96'>
         <Canvas camera={{ fov: 75, position: [600, 400, 200]}}>
-          <OrbitControls enableZoom={true} enableRotate={true} enablePan={true} />
+          <perspectiveCamera/>
+          <OrbitControls enableZoom={true} maxDistance={900} enableRotate={true} enablePan={true} />
           <pointLight position={[1, 5, 1]} />
           <points ref={points}>
             <bufferGeometry attach="geometry">
@@ -61,16 +70,30 @@ export default function App() {
           </points>
           <mesh ref={box} position = {[200,10,50]}>
             <boxGeometry attach="geometry" args={[20, 20, 100]}  />
-            <meshStandardMaterial attach="material" color={0x0ff000} />
+            <meshStandardMaterial attach="material" color={'#964B00'} opacity={0.8} transparent={true}/>
           </mesh>
-          <mesh ref={leftwall} position = {[0,0,50]}>
-            <boxGeometry attach="geometry" args={[20, 1, 100]}  />
-            <meshStandardMaterial attach="material" color={0x0ff000} />
+          <mesh ref={floor} position = {[200,0,75]}>
+            <boxGeometry attach="geometry" args={[400, 1, 150]}  />
+            <meshStandardMaterial attach="material" color={'#964B00'} opacity={0.04} transparent={true}/>
+          </mesh>
+          <mesh ref={backwall} position = {[0,150,75]}>
+            <boxGeometry attach="geometry" args={[1, 300, 150]}  />
+            <meshStandardMaterial attach="material" color={'#964B00'} opacity={0.04} transparent={true}/>
+          </mesh>
+          <mesh position = {[200,150,150]}>
+            <boxGeometry attach="geometry" args={[400, 300, 1]}  />
+            <meshStandardMaterial attach="material" color={'#964B00'} opacity={0.04} transparent={true}/>
+          </mesh>
+          <mesh position = {[200,150,0]}>
+            <boxGeometry attach="geometry" args={[400, 300, 1]}  />
+            <meshStandardMaterial attach="material" color={'#964B00'} opacity={0.04} transparent={true}/>
           </mesh>
         </Canvas>
       </div>
-      
     </div>
+
+    </div>
+    
    
   )
 }
--- convert.py
+++ convert.py
@@ -5,10 +5,12 @@ import json
 
 dir = r"C:\Users\jonam\Desktop\4th year project\10.3 - Reduced"
 json_dict = {}
+mins = pd.Series(data = {"Points:0":0.000403,"Points:1":0.000116,"Points:2":-0.003254})
+
 for filename in listdir(dir):
     print(filename)
     df = pd.read_csv(dir+"/{}".format(filename),usecols=["Points:0","Points:1","Points:2"])
-
+    df = df - mins
     coords = df.to_numpy().flatten()
     coords = coords*1000
     coords = coords.round(2).tolist()
@@ -17,3 +19,4 @@ for filename in listdir(dir):
 
 with open("./public/3d.json", "w") as outfile: 
     json.dump(json_dict, outfile)
+
